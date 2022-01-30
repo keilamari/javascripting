051fe3e932e4a7409be923a516942e2c9a8d3fb5
Keila Kopvillem <kopvillem.k@gmail.com>
2022-01-30 13:21:06 +0000

    for loop javascripting workshopper

--- for-loop.js
+++ for-loop.js
@@ -1,5 +1,8 @@
-let total = 0;
-let limit = 10;
-for (let i = 0; i < limit; i+=) {
-  console.log(total + i)
+var total = 0;
+var limit = 10;
+
+for(let i = 0; i < limit; i++) {
+  total += i;
 }
+
+console.log(total);
